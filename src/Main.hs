95eaaac1df1b08c4cfcc62d81043508d9342fb53
Lucas <lucass@gmail.com>
2016-02-10 20:06:08 -0200

    Testing if lines intersects

--- src/Main.hs
+++ src/Main.hs
@@ -4,50 +4,74 @@ module Main (main) where
 -- All non intersecting, non rotated lines for points --
 --                                                    --
 --                    1,3 2,3                         --
---                  ___x___x___                       --
+--             0,3 .___x___x___                       --
 --                 |           |                      --
 --             0,2 x           x 3,2                  --
 --                 |           |                      --
 --             0,1 x           x 3,1                  --
---                 |___x___x___|                      --
+--                 |___x___x___|.3,0                  --
 --                    1,0 2,0                         --
 --------------------------------------------------------
 
-data Point' = Point Int Int
+data Point' = Point {x :: Float, y :: Float} deriving (Show, Eq)
 
-topRight = Point 1 3
-topLeft = Point 2 3
-rightTop = Point 3 2
-rightBottom = Point 3 1
-bottomRight = Point 2 0
-bottomLeft = Point 1 0
-leftBottom = Point 0 1
-leftTop = Point 0 2
+topLeft = Point 1.0 3.0
+topRight = Point 2.0 3.0
+rightTop = Point 3.0 2.0
+rightBottom = Point 3.0 1.0
+bottomRight = Point 2.0 0.0
+bottomLeft = Point 1.0 0.0
+leftBottom = Point 0.0 1.0
+leftTop = Point 0.0 2.0
 
 thePoints :: [Point']
 thePoints = [topRight, topLeft, rightTop, rightBottom, bottomRight, bottomLeft, leftBottom, leftTop]
 
-data LineSegment' = LineSegment Point' Point'
+data LineSegment' = LineSegment {start :: Point', end :: Point'} deriving (Show, Eq)
 
-aLine = LineSegment topRight bottomRight
-anotherLine = LineSegment leftTop rightTop
+allPossibleLinesFor :: [Point'] -> [LineSegment']
+allPossibleLinesFor [] = []
+allPossibleLinesFor (p:pTail) = allLinesForFirstPoint ++ allPossibleLinesFor pTail where
+ allLinesForFirstPoint = map (\otherPoint -> LineSegment p otherPoint) pTail
 
-pointToString :: Point'->String
-pointToString  (Point x y) = "(" ++ show x ++ "," ++ show y ++ ")"
+pointIsInsideRectangle :: Point' -> Bool
+pointIsInsideRectangle (Point x y) = (x >=0) && (x<=3) && (y>=0) && (y<=3)
 
-lineToString :: LineSegment'->String
-lineToString (LineSegment start end) = "[" ++ pointToString start ++ "->" ++ pointToString end ++ "]"
+-- line equation y = ax + b
+aFor :: LineSegment' -> Float
+aFor (LineSegment (Point x y) (Point x2 y2)) = (y2 - y) / (x2 - x)
 
-linesToString :: [LineSegment'] -> String
-linesToString lines = foldr (++) "" (map lineToString lines )
+bFor :: LineSegment' -> Float
+bFor line@(LineSegment (Point x y) (Point x2 y2)) = y - (aFor line) * x
 
-allPossibleLines :: [Point'] -> [LineSegment']
-allPossibleLines [] = []
-allPossibleLines (p:pTail) = (map (\otherPoint -> LineSegment p otherPoint) pTail) ++ allPossibleLines pTail
+maybeCommonPointFor :: LineSegment' -> LineSegment' -> Maybe Point'
+maybeCommonPointFor (LineSegment l1p1 l1p2) (LineSegment l2p1 l2p2)
+  | l1p1 == l2p1 = Just l1p1
+  | l1p1 == l2p2 = Just l1p1
+  | l1p2 == l2p1 = Just l1p2
+  | l1p2 == l2p2 = Just l1p2
+  | otherwise = Nothing
 
-calc::String
-calc= linesToString $ allPossibleLines thePoints
+commonPointExistFor :: LineSegment' -> LineSegment' -> Bool
+commonPointExistFor l1 l2 = (maybeCommonPointFor l1 l2) /= Nothing
+
+intersectionPointFor :: LineSegment' -> LineSegment' -> Maybe Point'
+intersectionPointFor l1 l2
+  | commonPointExistFor l1 l2 = maybeCommonPointFor l1 l2
+  | (aFor l1) == (aFor l2) = Nothing
+  | otherwise = Just (Point x y)
+    where
+      aForL1 = aFor l1
+      bForL1 = bFor l1
+      x = ((bFor l2) - bForL1) / (aForL1 - (aFor l2))
+      y = aForL1 * x + bForL1
+
+maybePointIsInsideRectangle :: Maybe Point' -> Bool
+maybePointIsInsideRectangle Nothing = False
+maybePointIsInsideRectangle (Just intersection) = pointIsInsideRectangle intersection
+
+lineSegmentsIntersect :: LineSegment' -> LineSegment' -> Bool
+lineSegmentsIntersect l1 l2 = maybePointIsInsideRectangle $ intersectionPointFor l1 l2
 
 main = do
-  putStrLn calc
-  putStrLn $ show (length (allPossibleLines thePoints))
+  putStrLn $ show (intersectionPointFor (LineSegment topRight bottomLeft) (LineSegment topLeft bottomRight))
